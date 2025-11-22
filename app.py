26e1d8ef179b8639c17c5604fb1f3bd3ac509467
Ramji Lal Jhanginia <ramji.iiitdm2011@gmail.com>
2025-11-22 09:19:55 +0000

    Added Flask API and prediction script

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from src.components.data_ingestion import DataIngestion
-from src.components.data_transformation import DataTransformation
-from src.components.model_trainer import ModelTrainer
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load artifacts
+model = pickle.load(open('artifacts/model.pkl', 'rb'))
+preprocessor = pickle.load(open('artifacts/preprocessor.pkl', 'rb'))
+
+@app.route('/', methods=['GET'])
+def home():
+    return "✅ Student Performance Prediction API is Running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    df = pd.DataFrame([data])
+    transformed = preprocessor.transform(df)
+    result = model.predict(transformed)
+    return jsonify({"prediction": float(result[0])})
 
 if __name__ == "__main__":
-    obj = DataIngestion()
-    train_path, test_path = obj.initiate_data_ingestion()
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
-    transform = DataTransformation()
-    X_train, X_test, y_train, y_test, _ = transform.initiate_data_transformation(train_path, test_path)
-
-    trainer = ModelTrainer()
-    best_model, best_score = trainer.initiate_model_training(X_train, y_train, X_test, y_test)
-
-    print("Best Model:", best_model)
-    print("R2 Score:", best_score)
